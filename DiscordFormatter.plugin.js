dac1f1ffa137b37cc38116e8dfe01a7b84faa19d
jerbear2008 <jeremyrwoolley@gmail.com>
2022-01-08 17:52:36 -0800

    Create DiscordFormatter.plugin.js

---
+++ DiscordFormatter.plugin.js
@@ -0,0 +1,10 @@
+/**
+ * @name DiscordFormatter
+ * @description Discord Formatter is a tool that helps you format your discord messages. It supports colored text with codeblocks.
+ * @version 0.1
+ * @author jerbear
+ * @authorLink https://github.com/jerbear2008
+ * @authorId 787042660724113408
+ * @website https://jerbear2008.github.io/discord-formatter
+ * @source https://github.com/jerbear2008/discord-formatter
+ */
